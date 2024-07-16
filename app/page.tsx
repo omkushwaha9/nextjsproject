1e39202a14d6b2cd072f6c868aaae92ecf18b63f
omkushwaha9 <omkushwaha2005@gmail.com>
2024-07-16 19:39:36 +0530

    functions

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,16 @@
 // main page
+import Link from "next/link";
 export default function Home(){
-    return <h1> Home Page! </h1>
+    return (
+        <>
+        <h1>
+            Welcome home
+        </h1>
+        <Link href="/">Home</Link> <br/>
+        <h1>Product List</h1>
+        <h2><Link href="products/1">Product 1</Link></h2>
+        <h2><Link href="products/2">Product 2</Link></h2>
+        <h2><Link href="products/3">Product 3</Link></h2>
+        </>
+    );
 }
